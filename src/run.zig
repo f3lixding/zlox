4601544fba7e5e0ee300f3d0b2905e64a4fa8a70
Felix Ding <felixding1989@gmail.com>
2024-12-06 12:03:53 -0500

    Populates run file logic

--- src/run.zig
+++ src/run.zig
@@ -1,9 +1,17 @@
 const std = @import("std");
 
-pub fn run() !void {}
+pub fn run(code: []const u8) !void {
+    _ = code;
+}
 
-pub fn run_file(path: []const u8) !void {
-    _ = path;
+pub fn run_file(alloc: std.mem.Allocator, max_bytes: usize, path: []const u8) !void {
+    const file = try std.fs.cwd().openFile(path, .{});
+    defer file.close();
+
+    const code = try file.readToEndAlloc(alloc, max_bytes);
+    run(code) catch |err| {
+        std.debug.print("Error running file {s}: {s}\n", .{ path, err });
+    };
 }
 
 pub fn run_prompt() !void {
@@ -14,5 +22,8 @@ pub fn run_prompt() !void {
     var buf: [1024]u8 = undefined;
     while (try in_stream.readUntilDelimiterOrEof(&buf, '\n')) |line| {
         std.debug.print("> {s}\n", .{line});
+        run(line) catch |err| {
+            std.debug.print("Error running line {s}: {s}\n", .{ line, err });
+        };
     }
 }
