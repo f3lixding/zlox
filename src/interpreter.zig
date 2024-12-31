7b7cca7022d5cd37ef4e2b148106d085f3810157
Felix Ding <felixding1989@gmail.com>
2024-12-31 18:30:53 -0500

    Adds tests for binary expr evaluation

--- src/interpreter.zig
+++ src/interpreter.zig
@@ -9,16 +9,17 @@ pub const InterpreterError = error{
 pub const Interpreter = struct {
     pub fn evaluate(self: *Interpreter, expr: *const Expr) InterpreterError!Literal {
         return switch (expr.*) {
-            .LITERAL => |*l| l,
-            .UNARY => |*u| self.evaluate(u.expr),
+            .LITERAL => |l| l,
+            // TODO: enable evaluation of unary expression
+            .UNARY => |_| Literal{ .TRUE = {} },
             .BINARY => |*b| {
                 const left = try self.evaluate(b.left);
                 const right = try self.evaluate(b.right);
                 const op = &b.operator;
                 switch (op.*) {
                     .DOUBLE_EQUAL => {
-                        const left_tag = std.meta.activeTag(left.*);
-                        const right_tag = std.meta.activeTag(right.*);
+                        const left_tag = std.meta.activeTag(left);
+                        const right_tag = std.meta.activeTag(right);
                         if (left_tag != right_tag) return Literal{ .FALSE = {} };
                         switch (left_tag) {
                             .NUMBER => {
@@ -38,8 +39,8 @@ pub const Interpreter = struct {
                         }
                     },
                     .BANG_EQUAL => {
-                        const left_tag = std.meta.activeTag(left.*);
-                        const right_tag = std.meta.activeTag(right.*);
+                        const left_tag = std.meta.activeTag(left);
+                        const right_tag = std.meta.activeTag(right);
                         if (left_tag != right_tag) return Literal{ .TRUE = {} };
                         switch (left_tag) {
                             .NUMBER => {
@@ -59,8 +60,8 @@ pub const Interpreter = struct {
                         }
                     },
                     .GREATER => {
-                        const left_tag = std.meta.activeTag(left.*);
-                        const right_tag = std.meta.activeTag(right.*);
+                        const left_tag = std.meta.activeTag(left);
+                        const right_tag = std.meta.activeTag(right);
                         if (left_tag != right_tag) return error.OperationNotSupported;
                         switch (left_tag) {
                             .NUMBER => {
@@ -73,8 +74,8 @@ pub const Interpreter = struct {
                         }
                     },
                     .GREATER_EQUAL => {
-                        const left_tag = std.meta.activeTag(left.*);
-                        const right_tag = std.meta.activeTag(right.*);
+                        const left_tag = std.meta.activeTag(left);
+                        const right_tag = std.meta.activeTag(right);
                         if (left_tag != right_tag) return error.OperationNotSupported;
                         switch (left_tag) {
                             .NUMBER => {
@@ -87,8 +88,8 @@ pub const Interpreter = struct {
                         }
                     },
                     .LESS => {
-                        const left_tag = std.meta.activeTag(left.*);
-                        const right_tag = std.meta.activeTag(right.*);
+                        const left_tag = std.meta.activeTag(left);
+                        const right_tag = std.meta.activeTag(right);
                         if (left_tag != right_tag) return error.OperationNotSupported;
                         switch (left_tag) {
                             .NUMBER => {
@@ -101,8 +102,8 @@ pub const Interpreter = struct {
                         }
                     },
                     .LESS_EQUAL => {
-                        const left_tag = std.meta.activeTag(left.*);
-                        const right_tag = std.meta.activeTag(right.*);
+                        const left_tag = std.meta.activeTag(left);
+                        const right_tag = std.meta.activeTag(right);
                         if (left_tag != right_tag) return error.OperationNotSupported;
                         switch (left_tag) {
                             .NUMBER => {
@@ -115,8 +116,8 @@ pub const Interpreter = struct {
                         }
                     },
                     .MINUS => {
-                        const left_tag = std.meta.activeTag(left.*);
-                        const right_tag = std.meta.activeTag(right.*);
+                        const left_tag = std.meta.activeTag(left);
+                        const right_tag = std.meta.activeTag(right);
                         if (left_tag != right_tag) return error.OperationNotSupported;
                         switch (left_tag) {
                             .NUMBER => {
@@ -128,8 +129,8 @@ pub const Interpreter = struct {
                         }
                     },
                     .PLUS => {
-                        const left_tag = std.meta.activeTag(left.*);
-                        const right_tag = std.meta.activeTag(right.*);
+                        const left_tag = std.meta.activeTag(left);
+                        const right_tag = std.meta.activeTag(right);
                         if (left_tag != right_tag) return error.OperationNotSupported;
                         switch (left_tag) {
                             .NUMBER => {
@@ -141,8 +142,8 @@ pub const Interpreter = struct {
                         }
                     },
                     .SLASH => {
-                        const left_tag = std.meta.activeTag(left.*);
-                        const right_tag = std.meta.activeTag(right.*);
+                        const left_tag = std.meta.activeTag(left);
+                        const right_tag = std.meta.activeTag(right);
                         if (left_tag != right_tag) return error.OperationNotSupported;
                         switch (left_tag) {
                             .NUMBER => {
@@ -154,8 +155,8 @@ pub const Interpreter = struct {
                         }
                     },
                     .STAR => {
-                        const left_tag = std.meta.activeTag(left.*);
-                        const right_tag = std.meta.activeTag(right.*);
+                        const left_tag = std.meta.activeTag(left);
+                        const right_tag = std.meta.activeTag(right);
                         if (left_tag != right_tag) return error.OperationNotSupported;
                         switch (left_tag) {
                             .NUMBER => {
@@ -170,9 +171,8 @@ pub const Interpreter = struct {
                 }
             },
             .GROUPING => |*g| self.evaluate(g.expr),
-            .TERNARY => |*t| {
-                _ = t;
-            },
+            // TODO: enable evaluation of ternary expression
+            .TERNARY => |_| Literal{ .TRUE = {} },
         };
     }
 
@@ -185,4 +185,45 @@ pub const Interpreter = struct {
     }
 };
 
-test "truthy falsey" {}
+test "truthy falsey" {
+    const literal_true = Literal{ .TRUE = {} };
+    std.debug.assert(Interpreter.isTruthy(literal_true));
+}
+
+test "evaluate binary" {
+    // Evaluating just the literal should return the literal
+    const literal_string = Literal{ .STRING = "hello" };
+    var string_expr = Expr{ .LITERAL = literal_string };
+    var interpreter = Interpreter{};
+    var eval_res = try interpreter.evaluate(&string_expr);
+    std.debug.assert(std.mem.eql(u8, eval_res.STRING, "hello"));
+    // Evaluating a binary expr that compares two things that are the same
+    const literal_hello = Literal{ .STRING = "hello" };
+    var hello_expr = Expr{ .LITERAL = literal_hello };
+    var eql_eql_str = Expr{ .BINARY = .{
+        .left = &string_expr,
+        .operator = .DOUBLE_EQUAL,
+        .right = &hello_expr,
+    } };
+    eval_res = try interpreter.evaluate(&eql_eql_str);
+    std.debug.assert(std.meta.activeTag(eval_res) == .TRUE);
+    // Evaluating a binary expr that compares two thigns that are not the same
+    const literal_false = Literal{ .FALSE = {} };
+    var false_expr = Expr{ .LITERAL = literal_false };
+    var false_binary_expr = Expr{ .BINARY = .{
+        .left = &eql_eql_str,
+        .operator = .DOUBLE_EQUAL,
+        .right = &false_expr,
+    } };
+    eval_res = try interpreter.evaluate(&false_binary_expr);
+    std.debug.assert(std.meta.activeTag(eval_res) == .FALSE);
+    // Evaluating a binary expr that compares two things that are not of the same type
+    var num_expr = Expr{ .LITERAL = .{ .NUMBER = 3.2 } };
+    var str_num_bin_expr = Expr{ .BINARY = .{
+        .left = &hello_expr,
+        .operator = .GREATER,
+        .right = &num_expr,
+    } };
+    const eval_res_two = interpreter.evaluate(&str_num_bin_expr);
+    std.debug.assert(std.meta.isError(eval_res_two));
+}
